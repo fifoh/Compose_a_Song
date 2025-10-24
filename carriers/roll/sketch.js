7284e037df21faef648a3dbfd966f3efebe126b2
fifoh <125158808+fifoh@users.noreply.github.com>
2025-10-24 15:06:17 +0100

    Update sketch.js

--- carriers/roll/sketch.js
+++ carriers/roll/sketch.js
@@ -696,7 +696,7 @@ function touchEnded() {
 
   let buttonClicked = false;
 
-  clickProximityX = windowWidth * 0.3 / numEllipses; // was 0.3, tried 0.1, 0.2
+  clickProximityX = windowWidth * 0.4 / numEllipses; // was 0.3
   for (let i = 0; i < ellipses.length; i++) {
     let ellipseData = ellipses[i];
     let dXLeft = abs(touchX - (ellipseData.centerX - clickProximityX));
@@ -904,6 +904,7 @@ function loadPresetSong() {
   }
 
 }
+
 
 
 
